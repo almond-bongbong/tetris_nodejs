2d9190f8065eb5b43e56ac215a5c4016cad06b69
cmlee <cmlee@mocobee.com>
2018-04-20 16:11:12 +0900

    tetris 프로젝트 nodejs 로 옮기는중

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+const router = require('./router/main')(app);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+const server = app.listen(port, () => {
+  console.log(`Server is running at ${port}`);
+});
