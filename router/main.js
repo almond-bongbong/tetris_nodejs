7225c9e636a497f61c5cdcc2bc38fd9744ed49df
cmlee <cmlee@mocobee.com>
2018-04-20 16:51:50 +0900

    api , DB 연동 필요

--- router/main.js
+++ router/main.js
@@ -1,5 +1,17 @@
-module.exports = (app) => {
+module.exports = (app, fs) => {
   app.get('/', (req,res) => {
-      res.render('./index.html')
+    // TODO : 점수 목록 GET 하는 로직 추가
+
+    res.render('index', {
+      title: "tetris",
+    });
+  });
+
+  // TODO : 점수 POST 하는 함수 개발 필요
+  app.get('/score', (req, res) => {
+    fs.readFile( __dirname + "/../data/" + "user.json", 'utf8', function (err, data) {
+      console.log( data );
+      res.end( data );
+    });
   });
 };
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const fs = require("fs");
 const port = 4000;
-const router = require('./router/main')(app);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -12,3 +14,13 @@ const server = app.listen(port, () => {
 });
 
 app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(session({
+  secret: '@#@$MYSIGN#@$#$',
+  resave: false,
+  saveUninitialized: true
+}));
+
+const router = require('./router/main')(app, fs);
